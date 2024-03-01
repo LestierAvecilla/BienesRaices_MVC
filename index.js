31a1c7e33a3e4f30eb532b75296ae7891f859623
LestierAvecilla <lestier.avecilla@my.unitec.edu.mx>
2024-03-01 17:54:15 -0600

    4

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 //const express = require('express');//common js/express
 
 import express from "express";
-import usuariosRoutes from "./routes/usuariosRoutes.js"; 
+import usuariosRoutes from "./routes/usuariosRoutes.js";
+import db from  './config/db.js'
 
 
 
 
 //Crea la app
 const app = express();
+
+// Conexion a la base de datos
+
+try {
+    await db.authenticate();
+    console.log('Conectado a la base de datos');
+} catch (error) {
+    console.log(error);
+}
 
 //habilitar pug
 app.set('view engine', 'pug')
